c08e5b18e0e0eb7387b1ab9411ffba8b1bba59aa
drew <drew@petipas.co>
2021-12-14 15:03:16 -0500

    Update

--- backup.sh
+++ backup.sh
@@ -2,10 +2,10 @@
 #2021-11-14 - Drew Petipas - This script will pull data using rsync over SSH to the backup server. This bash script is called as a cron job by root.
 
 # Call script for custom alert - alert that script has started. If I don't see an alert for success, I know something went wrong.
-bash /home/admin/backup/backup_alert_start.sh
+bash /usr/local/bin/backup/backup_alert_start.sh
 
 # Variables .conf
-source /home/admin/backup/backup.conf
+source /usr/local/bin/backup/backup.conf
 
 # Log testing start
 echo "$(date "+%F %T") [INIT] Script initializing, testing..." >> $log_location
@@ -24,7 +24,7 @@ if [ $? != 0 ]; then
 fi
 
 # Test if SSH is up
-ssh -q $remote_user@$remote_server exit
+ssh $remote_user@$remote_server exit
 if [ $? = 255 ]; then
 	echo "$(date "+%F %T") [ERROR $?] SSH is down." >> $log_location
 	exit 1
@@ -32,7 +32,7 @@ fi
 
 # Begin backup
 echo "$(date "+%F %T") [START] Backup started." >> $log_location
-rsync --quiet --archive --ignore-existing --delete-during --exclude $exclude --compress $remote_user@$remote_server:$remote_dir/* $backup_dir >> /dev/null
+rsync --archive --ignore-existing --delete-during --exclude $exclude --compress $remote_user@$remote_server:$remote_dir/* $backup_dir >> /dev/null
 rsync_exit=$?
 echo "$(date "+%F %T") [END] Backup ended." >> $log_location
 
@@ -40,7 +40,7 @@ echo "$(date "+%F %T") [END] Backup ended." >> $log_location
 if [ $rsync_exit = 0 ]; then
 	echo "$(date "+%F %T") [SUCCESS] Backup completed successfully." >> $log_location
 	# Call script for custom alert - alert that the backup was successful.
-	bash /home/admin/backup/backup_alert_success.sh
+	bash /usr/local/bin/backup/backup_alert_success.sh
 	exit 0
 else
 	echo "$(date "+%F %T") [ERROR $rsync_exit] Backup failed." >> $log_location
